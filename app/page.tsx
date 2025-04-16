2cf1f0bfa25497e6e062dbf7ffa8866c1560390e
elangyudha280 <elangyudharakasiwi2@gmail.com>
2025-04-16 10:25:03 +0700

    set component todos

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,44 @@ export default function Home() {
   // const {data} = useGetTodoCSR()
   console.log(data)
   return (
-  <section className="position-relative w-full p-5 h-[100vh] overflow-x-hidden overflow-y-auto bg-slate-900">
-   
-   {
-    isLoading ?
-    <p className="text-slate-50">
-    loading...
-  </p>
-  :
-  <p className="text-slate-50">
-  {data[0]?.title}
-</p>
-   }
+  <section className="position-relative w-full p-5 h-[100vh] overflow-x-hidden overflow-y-auto bg-slate-900 flex justify-center items-center">
+ 
+    {/* container */}
+    <section className="w-[450px]">
+
+
+    {/* input group */}
+    <div className="flex gap-4 ">
+      <input type="text" className="p-2 flex-1 outline-none  text-[14px] font-medium bg-transparent border-b-2 border-b-white" placeholder="Add todo..." />
+
+      <button className="p-2 bg-lime-700 rounded-md">
+        Add
+      </button>
+    </div>
+
+    {/* list todo */}
+    <div className="flex flex-col h-[300px] overflow-x-hidden overflow-y-auto gap-3 mt-4 ">
+      {
+        isLoading ? 
+        <p className="text-center text-white">
+          Loading...
+        </p>
+        :
+        data?.map((el:any)=>{
+         return (
+          <div key={el.id} className="p-5 bg-blue-950 w-full rounded-md">
+            <p className="m-0 p-0 text-white">
+            {el?.title}
+            </p>
+          </div>
+         ) 
+        })
+
+      }
+    </div>
+
+    </section>
+    
    
   </section>
   );
--- services/todos/get-todo.ts
+++ services/todos/get-todo.ts
@@ -4,7 +4,8 @@ import { interCeptorAxiosClient } from "@/lib/hooks/use-server-axios";
 const getTodos = async ()=>{
     const axios =await interCeptorAxiosClient()
     try {
-        const response = await axios.get('todos')
+        const response = await axios.get('todos?userId=1')
+        console.log(response)
         return response.data
     }
     catch(err:any){
